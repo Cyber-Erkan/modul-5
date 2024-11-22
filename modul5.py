07a40de326fc25909b156e54a3509859ccbdac6d
Cyber-Erkan <erik.ekryd@gmail.com>
2024-11-22 10:04:37 +0100

    first commit

---
+++ modul5.py
@@ -0,0 +1,26 @@
+import os
+os.system("cls")
+"""
+Gör ett litet högsta/lägsta talet spel som du kan avsluta med ‘q’ men som du också kan skriva siffor
+
+
+
+
+Skriv ut en viss längd av ett ord (word[:10])
+
+
+
+Gör ett program som kapar ord om det är för långt 
+    - gör så det läggs till “...” om ordet är för långt.
+"""
+
+"""
+text = input("Input a number:")
+
+if text.isdigit():
+    print("You wrote a number")
+else:
+    print("you did not write a number")
+"""
+
+import
